27495698f2f3150b4b4469151ddcea91f4e1e50c
andrzejzmuda <andrzejzmuda@hotmail.com>
2022-09-21 18:51:20 +0200

    upload image & authorisation

--- back/main.py
+++ back/main.py
@@ -1,14 +1,23 @@
+from turtle import pos
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from db import models
 from db.database import engine
-from routers import user
+
+from routers import user, post
+from auth import authentication
+
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(post.router)
+app.include_router(authentication.router)
 
 @app.get("/")
 def root():
     return "Hello World"
 
 models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
--- back/routers/schemas.py
+++ back/routers/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class UserBase(BaseModel):
     username: str
@@ -11,3 +12,34 @@ class UserDisplay(BaseModel):
     email: str
     class Config():
         orm_mode = True
+
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str #relative or absolute
+    caption: str
+    creator_id: int
+
+
+# For post display
+class User(BaseModel):
+    username: str
+    class Config():
+        orm_mode = True
+
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
+    class Config():
+        orm_mode = True
+
+
+class UserAuth(BaseModel):
+    id: int
+    username: str
+    email: str
